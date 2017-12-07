f1f348e25da159d0d8605f7c91387b93a188817d
dp <pantyuhind@gmail.com>
2017-12-08 01:35:38 +0300

    +

--- src/constants.js
+++
@@ -1,6 +0,0 @@
-export const CHECKBOX_GROUP = 'CHECKBOX_GROUP';
-export const DROPDOWN = 'DROPDOWN';
-export const INPUT = 'INPUT';
-
-export const CHOOSE_ALL = 'Выбрать все';
-export const UNCHOOSE_ALL = 'Снять выбор';
