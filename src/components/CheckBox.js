cfd2d808b78c8e3c3dd01424065d4a8459372bb6
dp <pantyuhind@gmail.com>
2017-12-08 11:19:51 +0300

    +

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -3,14 +3,14 @@ import React from 'react'
 export default function CheckBox(props) {
     const { value, checked, b } = props.data;
     return (
-        <label className="checkbox-group__control">
-            <div className="checkbox">
+        <div className="checkbox-group__control">
+            <label className="checkbox">
                 <input type="checkbox" checked={checked || false} name="" id="" onChange={props.onChange} />
                 <span className="checkbox__square" />
                 <span className={`checkbox__label ${b ? 'bold' : ''}`}>
                     { value }
                 </span>
-            </div>
-        </label>
+            </label>
+        </div>
     )
 }
